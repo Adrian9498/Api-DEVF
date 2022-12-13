10019bb0efde55c5c25a6b078eba757b82b35a41
Adrian Gutierrez <adgutierrez@ikeasistencia.com>
2022-12-12 20:45:24 -0600

    doenv y configuracion de la base

---
+++ src/database/database.js
@@ -0,0 +1,15 @@
+import  Sequelize  from "sequelize";
+import dotenv from 'dotenv';
+
+dotenv.config({path:'src/.env'})
+
+export const sequelize = new Sequelize(
+    process.env.DBNAME,
+    process.env.DBUSER,
+    process.env.DBPASSWORD,
+    {
+        host: process.env.DBHOST,
+        port: process.env.DBPORT,
+        dialect : 'postgres' //Hardcode
+    }
+);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import app from './app.js';
-//import {sequelize} from './database/database.js';
-//import dotenv from 'dotenv';
+import {sequelize} from './database/database.js';
+
 
 async function main(){
     try{
-        //dotenv.config({path:'src/.env'})
-        //await sequelize.sync({force:false});
-        //await sequelize.authenticate();
-        //console.log("Conexion a la base correcta");
+        
+        await sequelize.sync({force:false});
+        await sequelize.authenticate();
+        console.log("Conexion a la base correcta");
         app.listen(3000);
         console.log('Servidor ON');
         
