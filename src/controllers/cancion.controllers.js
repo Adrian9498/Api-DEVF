48172dd135effb28deccb43d05e05ea2c9501793
Daniel Hernandez <daniel244@live.com.mx>
2022-12-14 17:54:04 -0600

    GET: canciones menos populares

--- src/controllers/cancion.controllers.js
+++ src/controllers/cancion.controllers.js
@@ -43,3 +43,19 @@ export const getCancionesMasPopulares = async (req,res) => {
         res.status(500).json({"error": err.message});
     }
 }
+
+export const getCancionesMenosPopulares = async (req,res) => {
+    try{
+        const response = await Cancion.findAll(
+            {   
+                attributes: ['id_cancion','nombre','popularidad'], 
+                where: {
+                    popularidad:{[Op.lt]:30},
+                }
+            }
+        );
+        res.status(200).json(response);
+    }catch(err){
+        res.status(500).json({"error": err.message});
+    }
+}
--- src/routes/cancion.routes.js
+++ src/routes/cancion.routes.js
@@ -1,10 +1,10 @@
 import {Router} from 'express';
-import {getCancion} from '../controllers/cancion.controllers.js';
-import {getCancionesMasPopulares} from '../controllers/cancion.controllers.js';
+import {getCancion,getCancionesMasPopulares,getCancionesMenosPopulares} from '../controllers/cancion.controllers.js';
 const router = Router();
 
 router.get('/api/cancion', getCancion);
 router.get('/api/most_popular/cancion', getCancionesMasPopulares);
+router.get('/api/less_popular/cancion', getCancionesMenosPopulares);
 //router.get('/api/artista/:nombre', searchArtista);
 //router.post('/api/artista', createArtista);
 export default router;
